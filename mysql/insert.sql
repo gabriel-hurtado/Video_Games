46b6831699e3d342fb9ca466081ec92ebbbea571
Gabriel Hurtado <gabriel.j.hurtado@gmail.com>
2017-04-28 15:39:50 +0200

    Orders improved

---
+++ mysql/insert.sql
@@ -0,0 +1,3 @@
+#password encrypted with SHA256 and salt
+#it gives 64 characters. The salt use is chips and here is an example for potatoes as the password
+# fc9fa4cd2a7c9301d1fbaa781d6be577ba1442df97d5c0f6415e27d526b25cb0
--- mysql/table-client.sql
+++ mysql/table-client.sql
@@ -1,6 +1,6 @@
 CREATE TABLE client (
 	id INTEGER NOT NULL UNIQUE AUTO_INCREMENT, #pkey
 	username CHAR(15) NOT NULL, #required for identification
-	password  CHAR(15) NOT NULL, #required for identification
+	password  CHAR(64) NOT NULL, #required for identification
     primary key (username)
 );
