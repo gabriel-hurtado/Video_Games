2f1514d9108e1d8d1cb5c9d4f45f039f0885ec92
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2017-04-28 16:04:04 +0200

    adding findById in AddressDao

--- videogames/src/dao/AddressDao.java
+++ videogames/src/dao/AddressDao.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import beans.Address;
+import beans.Client;
 
 public class AddressDao {
 	
@@ -48,6 +49,47 @@ public class AddressDao {
 			e.printStackTrace();
 		}
 		return addresses;
+	}
+	
+
+	public static Address findById(int id) {
+
+		Address u = null;
+		
+		Connection cnx=null;
+		try {
+			try {
+				cnx = ConnectionDB.getInstance().getCnx();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			//Requete
+			String sql = "SELECT id, address_line, city, country, owner FROM address WHERE id = ?";
+			PreparedStatement ps = cnx.prepareStatement(sql);
+			ps.setInt(1, id);
+			
+			//Execution et traitement de la r�ponse
+			ResultSet res = ps.executeQuery();
+			
+			while(res.next()){
+				Address newAddress = new Address(res.getInt("id"),
+						res.getString("address_line"),
+						res.getString("city"),
+						res.getString("country"),
+						res.getInt("owner"));
+				
+				u = newAddress;
+				break;
+			}
+			
+			res.close();
+					
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return u;
 	}
 	
 }
